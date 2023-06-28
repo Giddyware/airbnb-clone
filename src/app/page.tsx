5b31737796b6825396e448267a771a06738c0bc0
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-06-28 22:04:51 +0100

    🎉feat: build the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
-export default function Home() {
-  return <div className="text-2xl text-rose-500">Hello</div>;
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
+
+export default async function Home() {
+  const isEmpty = true;
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      // *change div to ClientOnly
+      <div>
+        <EmptyState showReset />
+      </div>
+    );
+  }
+  return (
+    //* replace div with ClientOnly component
+    <div className="text-2xl text-rose-500">
+      <Container>
+        <div className="grid gap-8 pt-24-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                currentUser={currentUser}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </div>
+  );
 }
