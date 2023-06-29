199833d587a2ba47846cd005a769765f80e64f54
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-06-29 02:21:58 +0100

    🛠fix: grid issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,36 @@
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
-  const isEmpty = true;
   const listings = await getListings();
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
-      // *change div to ClientOnly
-      <div>
+      <ClientOnly>
         <EmptyState showReset />
-      </div>
+      </ClientOnly>
     );
   }
   return (
-    //* replace div with ClientOnly component
-    <div className="text-2xl text-rose-500">
+    <ClientOnly>
       <Container>
-        <div className="grid gap-8 pt-24-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          {listings.map((listing: any) => {
+        <div
+          className=" pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8"
+        >
+          {listings.map((listing) => {
             return (
               <ListingCard
                 key={listing.id}
@@ -33,6 +41,6 @@ export default async function Home() {
           })}
         </div>
       </Container>
-    </div>
+    </ClientOnly>
   );
 }
